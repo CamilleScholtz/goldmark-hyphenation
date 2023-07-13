e7d34adc0587e08fa147829081d148ea1e4c071f
Camille Scholtz <admin@camille.sh>
2023-07-14 00:17:09 +0200

    second commit

--- hyphenation.go
+++ hyphenation.go
@@ -1,75 +1,64 @@
 package hyphenation
 
 import (
-	"log"
 	"os"
 	"strings"
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/ast"
-	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/util"
 
-	"github.com/speedata/hyphenation"
+	hyphenator "github.com/speedata/hyphenation"
 )
 
-type Hyphenation struct {
-	hyphenator *hyphenation.Lang
+type HyphenationConfig struct {
+	hyphenator *hyphenator.Lang
 }
 
 type HyphenationOption interface {
-	parser.Option
-	SetHyphenationOption(*Hyphenation)
+	SetHyphenationOption(*HyphenationConfig)
 }
 
 type withHyphenationFile struct {
-	value string
+	value *os.File
 }
 
-func (o *withHyphenationFile) SetParserOption(c *parser.Config) {
-	// This option doesn't affect the parser, so do nothing here.
-}
-
-func (o *withHyphenationFile) SetHyphenationOption(h *Hyphenation) {
-	filename := o.value
-	r, err := os.Open(filename)
+func (o *withHyphenationFile) SetHyphenationOption(h *HyphenationConfig) {
+	l, err := hyphenator.New(o.value)
 	if err != nil {
-		log.Fatal(err)
-	}
-	l, err := hyphenation.New(r)
-	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	h.hyphenator = l
 }
 
-func WithHyphenationFile(value string) HyphenationOption {
+func WithHyphenationFile(value *os.File) HyphenationOption {
 	return &withHyphenationFile{
 		value: value,
 	}
 }
 
 func NewHyphenation(opts ...HyphenationOption) goldmark.Extender {
-	h := &Hyphenation{}
+	h := &HyphenationConfig{}
 	for _, opt := range opts {
 		opt.SetHyphenationOption(h)
 	}
+
 	return h
 }
 
-func (h *Hyphenation) Extend(m goldmark.Markdown) {
+func (h *HyphenationConfig) Extend(m goldmark.Markdown) {
 	m.Renderer().AddOptions(renderer.WithNodeRenderers(
 		util.Prioritized(NewHyphenationHTMLRenderer(h.hyphenator), 500),
 	))
 }
 
 type HyphenationHTMLRenderer struct {
-	hyphenator *hyphenation.Lang
+	hyphenator *hyphenator.Lang
 }
 
-func NewHyphenationHTMLRenderer(hyphenator *hyphenation.Lang) *HyphenationHTMLRenderer {
+func NewHyphenationHTMLRenderer(hyphenator *hyphenator.Lang) *HyphenationHTMLRenderer {
 	return &HyphenationHTMLRenderer{
 		hyphenator: hyphenator,
 	}
